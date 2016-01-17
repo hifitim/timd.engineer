e90aadcd6212f94fda79352f6dcd971715818677
Tim Davis <tim.hifi@gmail.com>
2016-01-16 20:51:07 -0600

    Initial commit

---
+++ table_maker.py
@@ -0,0 +1,41 @@
+
+if __name__ == "__main__":
+    rows = 5
+    cols = 5
+    matrix = [[0 for x in range(rows)] for x in range(cols)]
+
+    matrix[0][0] =
+    matrix[0][1] =
+    matrix[0][2] =
+    matrix[0][3] =
+    matrix[0][4] =
+
+    matrix[1][0] =
+    matrix[1][1] =
+    matrix[1][2] =
+    matrix[1][3] =
+    matrix[1][4] =
+
+    matrix[2][0] =
+    matrix[2][1] =
+    matrix[2][2] =
+    matrix[2][3] =
+    matrix[2][4] =
+
+    matrix[3][0] =
+    matrix[3][1] =
+    matrix[3][2] =
+    matrix[3][3] =
+    matrix[3][4] =
+
+    matrix[4][0] =
+    matrix[4][1] =
+    matrix[4][2] = 
+    matrix[4][3] =
+    matrix[4][4] =
+
+    for k in range(5):
+        for j in range(5):
+            matrix[k][j] = '%s,%s'%(k,j)
+
+    print(matrix)
